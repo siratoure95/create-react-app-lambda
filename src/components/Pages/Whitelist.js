ed216fca59cfa4f8b612eb9656d45674f86d4c24
siratoure95 <siratoure95@gmail.com>
2021-11-28 17:55:48 -0800

    Need to to get submission button working

---
+++ src/components/Pages/Whitelist.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Whitelist() {
+
+     return (
+       <div>
+         <p>This is the Whitelist page.</p>
+       </div>
+     );
+
+ }
+
+ export default Whitelist;
---
+++ src/components/Whitelist.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+  
+const Whitelist = () => {
+  return (
+    <div>
+      <h1>Whitelist Page</h1> 
+    </div>
+  );
+};
+  
+export default Whitelist;
